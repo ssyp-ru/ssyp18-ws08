580489354c419cc466193af91f2330fd593a2b87
sorokin <sorokin-mich@yandex.ru>
2018-07-07 17:48:16 +0700

    merged old netgame and GUI

---
+++ src/main/kotlin/Game/main.kt
@@ -0,0 +1,20 @@
+package Game
+
+import org.newdawn.slick.AppGameContainer
+import org.newdawn.slick.SlickException
+import java.util.logging.Level
+import java.util.logging.Logger
+
+/*
+fun main(args: Array<String>) {
+    try {
+        val appgc: AppGameContainer
+        appgc = AppGameContainer(SimpleSlickGame("Simple Slick Game"))
+        //appgc.setDisplayMode(1280, 720, true)
+        appgc.setDisplayMode(640, 480, false)
+        appgc.start()
+    } catch (ex: SlickException) {
+        Logger.getLogger(SimpleSlickGame::class.java.name).log(Level.SEVERE, null, ex)
+    }
+}
+        */
