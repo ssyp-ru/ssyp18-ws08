48e1edf424e6f798c06aa07ef29ed32ba204941c
sorokin <sorokin-mich@yandex.ru>
2018-07-08 20:04:29 +0700

    added mapscreenshot and some jokes

--- src/main/kotlin/GUILobby/Back.kt
+++ src/main/kotlin/GUILobby/Back.kt
@@ -4,12 +4,12 @@ import GUI.Button
 import org.newdawn.slick.GameContainer
 import org.newdawn.slick.Image
 
-class Start(gc: GameContainer) : Button(gc,
-        imageCommon = Image("res/start1.png"),
-        imageLighted = Image("res/start2.png"),
-        imageClicked = Image("res/start3.png"),
+class Back(gc: GameContainer) : Button(gc,
+        imageCommon = Image("res/back1.png"),
+        imageLighted = Image("res/back2.png"),
+        imageClicked = Image("res/back3.png"),
         sizeX = gc.screenWidth.toFloat() / 3f,
         sizeY = gc.screenWidth.toFloat() / 8f,
         xButton = gc.screenWidth.toFloat() - gc.screenWidth.toFloat() / 3f,
-        yButton = gc.screenHeight.toFloat() - gc.screenWidth.toFloat() / 4f) {
+        yButton = gc.screenHeight.toFloat() - gc.screenWidth.toFloat() / 8f) {
 }
--- src/main/kotlin/GUILobby/Lobby.kt
+++ src/main/kotlin/GUILobby/Lobby.kt
@@ -8,10 +8,12 @@ import java.util.Arrays.asList
 
 class Lobby(val gc: GameContainer, val IsHost: Boolean, val networckobj: Network, val lobbyName: String) {
     val startButton = Start(gc)
-    val jpgNames = asList( "pepe","cat", "sun","cheb","gen","zaic")
+    val backButton = Back(gc)
+    val jpgNames = asList("pepe", "cat", "sun", "cheb", "gen", "zaic")
     val listOfPlayersIamge = Image("res/lobby.png")
     val rand = Random(Date().time)
     val placeHolderIamge = Image("res/lobbymems/" + jpgNames[rand.nextInt(jpgNames.size)] + ".jpg")
+    val miniMap = Image("res/map_screen.png")
     val xOfJPEG = gc.screenWidth / 40f
     val yOfJPEG = gc.screenHeight / 40f
     val lengthOfJPEG = gc.screenWidth / 5f
@@ -23,13 +25,17 @@ class Lobby(val gc: GameContainer, val IsHost: Boolean, val networckobj: Network
     ), false)
     val fontPlayerName = TrueTypeFont(java.awt.Font("Comic Sans MS", java.awt.Font.BOLD,
             gc.screenHeight / 50), false)
+    val miniMapSize = gc.screenHeight - gc.screenWidth.toFloat() / 4f - 1
 
     fun lobbyRender(g: Graphics) {
         if (IsHost) {
             startButton.draw(gc, gc.input.mouseX.toFloat(), gc.input.mouseY.toFloat())
         }
+        backButton.draw(gc, gc.input.mouseX.toFloat(), gc.input.mouseY.toFloat())
         listOfPlayersIamge.draw(xOfJPEG, yOfJPEG, lengthOfJPEG, heightOfJPEG)
         placeHolderIamge.draw(placeHolderX, placeHoldery, placeHolderLength, heightOfJPEG)
+
+        miniMap.draw(gc.screenWidth.toFloat() - miniMapSize, 0f, miniMapSize, miniMapSize)
         var playerNameShiftY = yOfJPEG + gc.screenHeight / 9f
         val playerNameShiftX = xOfJPEG + gc.screenWidth / 40f
         val players = networckobj.getPlayers()
@@ -42,11 +48,16 @@ class Lobby(val gc: GameContainer, val IsHost: Boolean, val networckobj: Network
                     playerNameShiftY)
             playerNameShiftY += gc.screenHeight / 20f
         }
+        g.color=Color.gray
+        g.drawString("Before launching game, we recommend you to get some paper bags",1f,gc.screenHeight-30f)
     }
 
     fun lobbyUpdate() {
         if (startButton.state == State.USED) {
             networckobj.startGame()
+        }
+        if (backButton.state == State.USED) {
+            error("back")
         }
     }
 }
